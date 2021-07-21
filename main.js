c1278d9456079c66286cf07f4a62ffa087d49512
Donghwan <donghwan.kim92@gmail.com>
2021-07-21 23:47:56 +0200

    second

--- main.js
+++ main.js
@@ -5,13 +5,21 @@ addBtn.addEventListener('click', () => {
   // let li = document.createElement('li');
   let titleInput = document.getElementById('titleInput').value;
   let contentInput = document.getElementById('contentInput').value;
-  let title = document.createElement('span')
-  let content = document.createElement('span')
+  let note = document.createElement('div');
+  let title = document.createElement('span');
+  let content = document.createElement('span');
+  let details = document.createElement('button');
 
   title.innerHTML = titleInput;
   content.innerHTML = contentInput;
-  noteContainer.appendChild(title);
-  noteContainer.appendChild(content);
+  note.appendChild(title);
+  note.appendChild(content);
+  note.appendChild(details);
+
+  details.innerText = 'Details'
+  note.className = 'note'
+
+  noteContainer.appendChild(note);
 
   // noteContainer.appendChild(li);
 });
